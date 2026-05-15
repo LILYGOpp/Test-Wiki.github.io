db700f5ad84eda630ce7fabbf534c9de6b29afda
puboy7 <@shiyizhiyu>
2026-05-15 11:41:16 +0800

    124

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'LILYGO Wiki',
   description: 'LILYGO - Happy to write',
   lastUpdated: true,
-  base: '/',
+  base: '/Test-Wiki.github.io/',
   ignoreDeadLinks: true,
 
   locales: {
